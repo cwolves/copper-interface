44826d17b37e9e83f47e5faac071f4d4b4931162
thatcher thornberry <thatcherthornberry@gmail.com>
2023-03-21 02:09:13 -0600

    update for new token auth

--- send_logs.py
+++ send_logs.py
@@ -40,21 +40,16 @@ def send_logs_to_cwolves(json_log_str, splunk_hec_token, splunk_host):
             log_data = log_data_dict[i : i + batch_size]
             log_data = json.dumps(log_data)
             try:
-                url = "https://copper.p.rapidapi.com/slash"
-                headers = {
-                    "X-RapidAPI-Key": "25bd864c53msha9976c156c6791ap1eb57djsn2eb7a61dedbc",
-                    "X-RapidAPI-Host": "copper.p.rapidapi.com"
-                }
+                url = "https://t7luua7qrcptuxv534pl4relem0cplzk.lambda-url.us-west-2.on.aws/"
                 response = requests.post(
                     url,
                     json={
                         "splunk_host": splunk_host,
                         "splunk_hec_token": splunk_hec_token,
                         "log_data": log_data,
-                        "copper_api_token": "",  # not used yet
+                        "api_token": "cw_SEIBdxmJewJnJav75jX9I3ftQFFfvPbW",
                         "log_type": "json",  # not used yet
                     },
-                    headers=headers,
                     # timeout=0.0000000001,
                 )
                 print(response.status_code, response.text)
